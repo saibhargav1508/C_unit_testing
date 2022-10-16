9c53b98ee65a1c95496a86d8f9e0ed75a438a2c8
Sai Bhargav Mandavilli <saibhargav1508@gmail.com>
2022-10-16 15:24:31 -0700

    Add files via upload

---
+++ test/customer.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "testy/customer.h"
+}
+
+TEST(customer, ok) {
+  ASSERT_EQ(customer_check(5), 1);
+}
+
+TEST(testy, not_ok) {
+  ASSERT_EQ(customer_check(0), 0);  
+}
+
